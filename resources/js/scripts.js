97617186ac667daa198cced8c2712ad531bd886c
Ryan Lee <rjlguitar@gmail.com>
2020-09-15 13:22:43 -0700

    connect cart to folder

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,7 @@
 const itemsContainer = document.getElementById('items')
+const itemList = document.getElementById('item-list')
+const cartQty = document.getElementById('cart-qty')
+const cartTotal = document.getElementById('cart-total')
 import data from './data.js'
 // the length of our data determines how many times this loop goes around
 for (let i=0; i<data.length; ++i) {
@@ -58,14 +61,19 @@ function addItem(name, price) {
 
 // show items
 function showItems() {
-    
+    const qty = getQty()
     console.log(`You have ${getQty()} items in your cart`)
-
+    cartQty.innerHTML = `You have ${qty} items in your cart`
+    let itemStr = ''
     for (let i = 0; i < cart.length; i += 1) {
         console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+        const { name, price, qty } = cart[i]
+
+        itemStr += `<li>${name} $${price} x ${qty} = ${qty * price} </li>`
     }
-    
+    itemList.innerHTML = itemStr
     console.log(`Total in cart: $${getTotal()}`)
+    cartTotal.innerHTML = `Total in cart: $${getTotal()}`
 }
 
 // get qty
